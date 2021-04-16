ce50e851e9ef763699d01b06b53f465bcc17624c
Jaz <montenegrojash@gmail.com>
2021-04-16 23:20:33 +0800

    D4

---
+++ project/1-modal/practice/main.js
@@ -0,0 +1,36 @@
+  // get modal element
+    let modal = document.querySelector('.modal');
+
+  // get modal btn
+    let modalBtn = document.querySelector('.modalBtn');
+
+  // get close btn
+    let closeBtn = document.getElementsByClassName('close');
+
+  // listen open click modal
+    modalBtn.addEventListener('click', openModal);
+
+  // listen outside click modal
+    window.addEventListener('click', outsideClick);
+
+  // openModal Function
+    function openModal(){
+      modal.style.display = 'block';
+    }
+
+  // closeModal Function
+    function closeModal(){
+      modal.style.display = 'none';
+    }
+
+  // outsideClick Function
+    function outsideClick(pop){
+      if (pop.target == modal){
+          modal.style.display = 'none';
+      }
+    }
+
+  // loop to get all class names
+    for (var i = 0; i < closeBtn.length; i++) {
+      closeBtn[i].addEventListener('click', closeModal);
+    }
