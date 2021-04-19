e9a9cff837148595aead995307a7c8e5bd23c3db
Jaz <montenegrojash@gmail.com>
2021-04-19 23:46:10 +0800

    D7

---
+++ project/2-Drag & Drop/main.js
@@ -0,0 +1,16 @@
+const fill = document.querySelector('.fill');
+const empties = document.querySelectorAll('.empty');
+
+// fill listener
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
+
+// Drag Functions
+function dragStart(){
+
+}
+
+funstion dragEnd(){
+
+}
+
