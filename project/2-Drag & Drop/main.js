2e649f8def3a2d5d426aa79ee6e8966acdf2abc4
Jaz <montenegrojash@gmail.com>
2021-04-21 23:32:57 +0800

    D9

--- project/2-Drag & Drop/main.js
+++ project/2-Drag & Drop/main.js
@@ -2,15 +2,44 @@ const fill = document.querySelector('.fill');
 const empties = document.querySelectorAll('.empty');
 
 // fill listener
-fill.addEventListener('dragstart', dragStart);
-fill.addEventListener('dragend', dragEnd);
+  fill.addEventListener('dragstart', dragStart);
+  fill.addEventListener('dragend', dragEnd);
+
+// Loop through empties and call drag events
+  for(const empty of empties){
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
+  }
+
 
 // Drag Functions
-function dragStart(){
+  function dragStart() {
+    this.className += ' hold';
+    setTimeout(() =>   this.className = 'invisible',0);
+  }
+  function dragEnd() {
+    this.className = 'fill';
+  }
 
-}
+  function dragOver(e) {
+    e.preventDefault();
+  }
 
-funstion dragEnd(){
+  function dragEnter(e) {
+    e.preventDefault();
+    this.className += ' hovered';
 
-}
+  }
+
+  function dragLeave() {
+    this.className = 'empty';
+  }
+
+  function dragDrop() {
+    this.className = 'empty';
+    this.append(fill);
+  }
+
 
