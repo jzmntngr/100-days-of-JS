8ca997d0164886ed93ec31e58b9bc1657ba08543
Jaz <montenegrojash@gmail.com>
2021-05-04 23:25:52 +0800

    D22

---
+++ project/3-Selection/main.js
@@ -0,0 +1,16 @@
+let boxes = document.querySelectorAll('.box');
+
+// boxes.forEach( box =>{
+  //   boxes.addEventListener('click', ()=>{
+  //     let target = document.querySelectorAll('');
+  //     box.classList.add('active');
+  //     console.log('jaja');
+  // })
+// })
+
+boxes.addEventListener('click', ()=>{
+  console.log('ajjaja');
+
+
+})
+
